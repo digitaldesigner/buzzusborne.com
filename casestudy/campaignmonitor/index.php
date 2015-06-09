75cb421fba1af5f789d338bc231cf86c5230b375
Buzz Usborne <hello@buzzusborne.com>
2015-06-09 19:16:55 +1000

    Campaign Monitor case study
    Loads of images yo

--- casestudy/campaignmonitor/index.php
+++ casestudy/campaignmonitor/index.php
@@ -10,60 +10,59 @@
 	</div>
 	
 	<div class="fullImage">
-		<img src="../_images/campaignmonitor/full_logo_blue.png" />
+		<img src="../_images/campaignmonitor/design-team.jpg" />
 	</div>
 
 	<div class="container">
 		<div class="project">
-			<p class="info"><strong>Above &amp; below/</strong> Something</p>
+			<p class="info"><strong>Above/</strong> My design team</p>
 			<p>I was the Creative Director for the Digital Design team at Campaign Monitor. Over the course of 3 years, I designed the brand, site and set the visual tone for all global marketing. During this time I had my hand in all areas of design, from print and advertising through to art direction and development.</p>
-			
-			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/business_cards.jpg" width="100%"></div>
-
-			<h5>Branding</h5>
-			<p class="info"><strong>Below/</strong> Note</p>			
 			<p>Focussing on giving the company a more &lsquo;human&rsquo; and modern feel, I designed the brand that Campaign Monitor uses today. Starting with the logo, I worked with typographer <a href="http://studioanthonylane.com/" target="_blank">Anthony Lane</a> to produce an identity that could be extended to our marketing efforts, both online and off.</p>
-			
-			<p>The &ldquo;marque&rdquo; &mdash; a combination of the letter &lsquo;M&rsquo;, an envelope and an intersecting graph &mdash; was developed to be a supporting asset, and used throughout the branded material.</p>
-		</div>	
+		</div>
 	</div>
 	
 	<div class="fullImage">
-		<img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/schematic_bw.png" width="100%" />
+			<img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/full_logo_blue.png" width="100%" />
 	</div>
 	
 	<div class="container">
 		<div class="project">
+			<div class="image half"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brand_sketch_01.jpg" width="100%"></div>
+			<div class="image half right"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brand_sketch_02.jpg" width="100%"></div>
+			<div class="image half close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brand_sketch_03.jpg" width="100%"></div>
+			<div class="image half right close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brand_sketch_04.jpg" width="100%"></div>
+			<div class="image close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/business_cards.jpg" width="100%"></div>
+
+			<p class="info"><strong>Below/</strong> Note</p>			
 			<p>One of my primary goals with the brand was to create a friendly and approachable image, whilst also remaining a respected leader in the field. <strong>Tone of voice</strong> played a big part in helping to achieve this &mdash; a playful tone can be seen in advertising.</p>
 			
 			<div class="image under"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/print_ad.jpg" width="100%"></div>
 			
 			<p>I also aimed to reinforce the playful, fun and human nature of the brand in supporting collateral &mdash; and always aimed to support the design community where possible. Here's how we played with the logo to produce a series of branded giveaway's.</p>
 			
-			<div class="image half"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brewtiful.jpg" width="100%"></div>
+			<div class="image half"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/sketchbook.jpg" width="100%"></div>
 			<div class="image half right"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/poster.jpg" width="100%"></div>
+			<div class="image close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/cb_tee.jpg" width="100%"></div>
 			<div class="image half close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/wallart.jpg" width="100%"></div>
-			<div class="image half right close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/sketchbook.jpg" width="100%"></div>
+			<div class="image half right close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/brewtiful.jpg" width="100%"></div>
+			<div class="image close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/ship_it.jpg" width="100%"></div>
+			
 			
 			<hr class="sectionBreak" />
 			<h5>Digital Marketing</h5>
 			<p class="info"><strong>Below/</strong> Note</p>			
 			<p>I was also responsible for the total redesign and re-build of the Campaign Monitor website, a 200+ page site that spans product marketing, blog and documentation. Launched at a similar time to the new logo, the site serves as an extension of the brand &mdash; friendly though a clear leader in the field.</p>
 			
-			<div class="image half"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/campaignmonitor_home.jpg" width="100%"></div>
-			<div class="image half right"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/campaignmonitor_customers.jpg" width="100%"></div>
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/customers_large.jpg" width="100%"></div>
+			<div class="image half close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/campaignmonitor_home.jpg" width="100%"></div>
+			<div class="image half right close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/case_study_small.jpg" width="100%"></div>
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/canvas_landing.jpg" width="100%"></div>
 			<div class="image half close"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/cm_contact.png" width="100%"></div>
 			<div class="image half right close under"><img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/cm_unlimited.png" width="100%"></div>
 			
 			<p>Proudly launched before &ldquo;flat design&rdquo; was a thing, and built on a custom PHP framework of my own design, the constantly-evolving site receives over 5 million unique visits a year.</p>
-		</div>	
-	</div>
-	
-	<div class="fullImage">
-		<img class="lazyload" data-sizes="auto" data-src="../_images/campaignmonitor/builder_landing.jpg" width="100%">
-	</div>
-	
-	<div class="container">
+
+
 		<dl class="outro">
 			<div class="col left">
 				<dt>Position</dt>
