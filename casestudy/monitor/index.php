b17d9350829ed5ec14451bedc21b91c2fc33b8e6
Buzz Usborne <hello@buzzusborne.com>
2015-06-13 14:59:53 +1000

    Monitor iconography

--- casestudy/monitor/index.php
+++ casestudy/monitor/index.php
@@ -17,19 +17,47 @@
 	include(path.'_includes/header.php');
 ?>
 		<div id="projectIntro">
-			<h1>Monitor</h1>
+			<h1>Monitor App</h1>
 			<span class="subscript">Creative Director <em>·</em> Art Direction / Iconography <em>·</em> 2014</span>
 		</div>
 	</div>
 	
 	<div class="fullImage">
-		<img src="../_images/campaignmonitor/cm_signage.jpg" />
+		<img src="../_images/monitor/iconography.png" />
 	</div>
 
 	<div class="container">
 		<div class="project">
-			<p class="info"><strong>Above/</strong> The Campaign Monitor logo featured in their Sydney offices</p>
-			<p>I was the Creative Director for the Digital Design team at Campaign Monitor. Over the course of 3 years, I designed the brand, site and set the visual tone for all global marketing. During this time I had my hand in all areas of design, from print and advertising through to art direction and development.</p>
+			<p class="info"><strong>Above/</strong> Iconography for the Monitor iPhone App</p>
+			<p>Calling on iconography experience from my time at Skype, I designed a set of icons for Campaign Monitor iPhone app &mdash; I also Art Directed and led the design of the app itself (<a href="http://itunes.apple.com/app/id855095172?ls=1&mt=8" target="_blank">App Store</a>).</p>
+			<p>Launched before the release of Apple iOS7 &mdash; which was set to have a distinctive new look to previous iOS's &mdash; the challenge was to match the app design to a new, and unknown native OS style.</p>
+
+			<h5>Guessing at &ldquo;native&rdquo; style</h5>
+			<p>I wanted the app to look as though it was native to the iOS7 ecosystem, and according to official Apple documentation, a big part of that was to rely on clean single-weight iconography. My intentions were to follow their guidelines, but also inject some &ldquo;Campaign Monitor&rdquo; personality into the iconography, where possible.</p>
+			
+			
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/sketches.png" width="100%" /></div>
+
+			<p>As always, the iconography started with a lot of sketching (above) to nail the concept behind each glyph. Following that I began experimenting with drawing single-weight line icons (a new style for me), whilst also attempting to give the artwork a common personality.</p>
+			<p>Below are some of the icons that didn't make the cut, but helped influence the final set. My main inspirations for this project were <a href="http://dribbble.com/mabu" target="_blank">Mads Burcharth</a> and <a href="http://dutchicon.com/" target="_blank">Dutch Icon</a> &mdash; both of whom combine single-weight line complexity, curves, angles and negative space to create unique-looking glyphs.</p>
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/concept_icons.png" width="100%" /></div>
+
+			<hr class="sectionBreak" />
+
+			<h5>Final Iconography</h5>
+			<p class="info"><strong>Below/</strong> Various examples of the main iconographic set, plus supporting (smaller) iconography</p>			
+			<p>It took 2 days to draw a set of 20 icons that not only looked at home within iOS7, but had a unique character Campaign Monitor could call their own. I was also able to use what I had learned at Skype regarding grids, to ensure the icons worked at any size without losing fidelity or quality.</p>
+			<p>You can read a more detailed review of <a href="https://www.campaignmonitor.com/blog/post/4080/designing-ios7-style-icons" target="_blank">my design process here.</a></p>
+
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/final_icons_blue.png" width="100%" /></div>
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/small_icons.png" width="100%" /></div>
+
+			<div class="image"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/nav_bar.png" width="100%" /></div>
+			
+			<p class="caption"><strong>Below/</strong> Iconography, along with the blank avatar I drew (modeled on my own head), being used in two of the key screens</p>
+			
+			<div class="image half under"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/iphone_map.jpg" width="100%" /></div>
+			<div class="image half under right"><img class="lazyload" data-sizes="auto" data-src="../_images/monitor/iphone_stats.jpg" width="100%" /></div>
 
 			<dl class="outro">
 				<div class="col left">
@@ -40,14 +68,14 @@
 				</div>
 				<div class="col middle">			
 					<dt>Responsibilities</dt>
-						<dd>Design</dd>
-						<dd>Management</dd>
-						<dd>Creative strategy</dd>
-						<dd>Art direction</dd>
+						<dd>Iconography</dd>
+						<dd>Art Direction</dd>
+						<dd>Project Management</dd>
 				</div>
 				<div class="col right">
 					<dt>More</dt>
-						<dd><a href="#">CampaignMonitor.com</a></dd>
+						<dd><a href="http://itunes.apple.com/app/id855095172?ls=1&mt=8" target="_blank">Download for iPhone</a></dd>
+						<dd><a href="https://www.campaignmonitor.com/monitor/" target="_blank">Monitor</a></dd>
 				</div>
 			</dl>
 	
