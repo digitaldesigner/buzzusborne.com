76b579401fca019773f563f737394425f609c43f
Buzz Usborne <hello@buzzusborne.com>
2015-06-22 10:56:33 +1000

    My age

--- age/index.php
+++ age/index.php
@@ -1,47 +1,41 @@
 <?php
 	define('path','../');
 	$hideNav=TRUE;
-	$navigation = array(
-		"next" => array(
-			"path"	=> "..",
-			"title"	=> "Home"
-		)
-	);
 	include(path.'_includes/header.php');
 ?>
 		
-		<div id="about">		
-			<p class="lead">I don't tell people my age. I've achieved a lot more than some people my age, but far less than others &mdash; so age seems irrelevant. What's important is experience.</p>
-			<hr />
+		<div id="about" class="timeline">		
+			<h3>So far&hellip;</h3>
 			
-			<p>I left home when I was 16, lived with a friend a sailed professionally with the sponsorship of the Royal Navy for several years. I travelled a lot, slept mostly in a car, and ended up living in the Mediterranean teaching at a sailing school. I also finished high-school during that period, splitting my time evenly between school and sailing. Because all my school exams were modular, I was able to complete my subjects in one or two months - and not turn up the rest of the year.</p>
+			<p class="lead">I left home when I was 16, lived with a friend a sailed professionally with the sponsorship of the Royal Navy for several years. I travelled a lot, slept mostly in a car, and ended up living in the Mediterranean teaching at a sailing school. I also finished high-school during that period, splitting my time evenly between school and sailing. Because all my school exams were modular, I was able to complete my subjects in one or two months, then travel the rest of the time.</p>
 			
-			<h6>Cornwall</h6>
-			<p>Having applied from abroad, I returned to the UK and started university at age 18, basically lying my way through the entry requirements. I spent half my first year at uni, the second half back in the Med, teaching.</p>
-
-			<p>In my second year I did an unpaid internship at a small design agency, then in my third year I started doing my own freelance design under the "company" Buzzgraphics. That work paid my rent and gave me commercial work to put in my portfolio. It was also enough to allow me return to work for the agency as a paid junior.</p>
+			<h6>University</h6>
+			<p>Having applied from a beach in Turkey, I returned to the UK and started university at age 18. On the application, the university expected qualifications that would require another year or more of study &mdash; so I just said I did, and was accepted anyway. Avoiding extra study means that I was a couple of years ahead of my peers, making me the youngest on the course.</p>
+				
+			<p>In my first year of uni, similar to my high-school days, I spent half my first year in the UK and the second half back in the Med, teaching &mdash; then gave up sailing for good. In my second year of uni I did an unpaid internship at a small design agency, then in my third I started doing my own freelance design work under the &ldquo;company&rdquo; name Buzzgraphics. The design I did on the side paid my rent and gave me commercial work to put in my portfolio. It was also enough to allow me return to work for the agency I had interned at previously, but this time as a paid junior.</p>
 			
-			<p>During my final show I got into an argument with a visiting Creative Director from London who thought my work was shit, I disagreed. He ended up giving me a job, which started the day after my degree ended. We lived in a shared house in the outskirts of London, with no belongings, working for less than the cost of travelling to work.</p>
+			<p>At the end of my degree, during my final show I got into an argument with a visiting Creative Director from London who thought my work was shit, I disagreed. We argued for a while, and he ended up giving me a job, starting the day after my degree ended. So my girlfriend and I moved to a shared house in the outskirts of London, with no belongings and started working for less than the cost of travelling to work.</p>
 			
 			<h6>London</h6>
-			<p>I worked at that agency doing print, branding and PR for a little over a year, and as one of three in the company, I rarely went home. I wanted to move into web, but didn't have enough experience to be represented by any reputable recruiters. I eventually managed to get an interview at a Digital agency by calling in a favour from a client, and lying my way through the meeting. I got the job.</p>
+			<p>My first experience of agency life was a baptism of fire. As one of only three in the company, I rarely went home &mdash; designing for a multitude of diciplines, from branding to print and PR, then liasing with clients directly. It was fun, but I really wanted to spend more time doing digital design, but didn't have enough experience to be represented by any reputable recruiters. After a year of work, I managed to score an interview at a top digital agency by calling in a favour from a client &mdash; I didn't have much experience, but I got the job.</p>
 
-			<p>Because I was used to working with clients and could do things other than digital, I ended up being given much bigger, more client-facing jobs. I regularly flew out to pitch, run workshops and work with clients internationally — so when the agency was bought by a larger company, people assumed my seniority and I continued on as a senior designer/art director. </p>
+			<p>Because I was used to working with clients from the time at my previous agency, I ended up being given much bigger, more client-facing jobs. I regularly flew overseas to pitch, run workshops and work with clients &mdash; at only 23, it was a tough learning experience. But by the time the agency was bought by a larger agency, people assumed my seniority based on the work I'd done, so I continued as a senior designer/art director. It was at this point that I started leading my own team, and when I stopped telling people my age.</p>
 
-			<p>During my time in London, I had also been continuing my freelance as Buzzgraphics, serving bigger clients I'd met as a result of working at agencies. I had also been building Prevue on the side.</p>
+			<p>During the time spent in London, I had also been continuing my freelance work, serving bigger clients I'd met as a result of working at agencies&hellip; it meant working evening and weekends, but helped pay the rent. As my freelance work became increasingly digital, I had also started building a small app to help me present that work to my clients, which I called Prevue.</p>
 
-			<p>The month that the recession hit the UK, I quit my job. I needed a new challenge, and had finally been accepted by the recruiter who'd rejected my several years earlier. Immediately after leaving my permanent job, I set up a Limited company under my own name and began working for myself. I did hundreds of design jobs in various disciplines, and also did a few longer in-house jobs for companies like Skype, and one 3 month contract in Australia as a temporary Creative Director.</p>
+			<p>The month the recession hit the UK, I decided to leave my job. My role had shifted into more management than design, and I felt that I needed a new creative challenge. It was arguably a risky move for a 24 year old &mdash; but fortunately as soon as announcing my resignation, I was approached to be represented by one of the recruiters who'd rejected me several years earlier. They kept the work coming thick and fast, so I set up a registered company, and began working for myself. I did hundreds of design jobs in various disciplines, and also a few longer in-house jobs as Art Director for companies like Skype, and one 3 month contract in Australia as a short-term Creative Director.</p>
 
 			<h6>Sydney</h6>
 
-			<p>Having spent a bit of time in Australia, both Sam and I decided to leave London and emigrate. We sold everything we owned and moved to Sydney, where I took a position at any agency that offered a permanent visa. Though at the time, the move was a step backwards for me in salary and seniority — I'd made the decision to focus on lifestyle instead of career for a while. Later that year, I sold my company in the UK and stopped doing freelance work altogether.</p>
+			<p>After just a brief time in Australia, my fiancé and I decided to leave London and emigrate. London was cool, but we were exhausted. So we sold everything we owned and moved to Sydney, where I took a position at an agency that offered a permanent visa. For a while, I stopped chasing titles and focussed on lifestyle instead of career &mdash; which meant selling my company in the UK, and stopping freelance altogether.</p>
 
-			<p>After almost a year in Sydney, I was approached by a software firm who had been impressed by my work on Prevue. Though initially hesitant to work "client-side", I accepted a role as Creative Director, tempted by the prospect of building my own design team and progressing the product and visual design of the company.</p>
+			<p>After almost a year in Sydney, I was approached by a software firm who asked whether I'd consider helping them grow their design presence. Though initially hesitant to work "client-side", I accepted a role as Creative Director, tempted by the prospect of building my own design team and progressing the product and visual design of the company.</p>
 
-			<p>I worked there for 4 years, longer than anywhere I'd worked previously. I built two design teams and was personally responsible for designing a number of high profile briefs, and art directing countless others. In parallel to working as Creative Director, I had spent almost every night working on Prevue — which over the four years had gone from a side project to a profitable business, thanks largely to the things I'd learned whilst working at a startup.</p>
+			<p>I worked there for 4 years. I built two design teams and was responsible for designing a number of high profile briefs, and art directing countless others. In parallel to working as Creative Director, I also spent most nights working on Prevue &mdash; which gave me a creative outlet as my role became increasingly management focussed. Over the course of four years, I turned Prevue from a side project into a profitable business used by over 20,000 &mdash; thanks largely to the things I'd learned whilst working at a startup.</p>
 			
 			<h6>Now</h6>
-			<p>I turned <strong>30</strong> this year.</p>
+			<p>Four years later, and in a similar fashion to what happened in London, I've decided to leave my full-time role to find another creative challenge. For now, that challenge is to grow Prevue &mdash; but in reality, I don't really know what's next. I've spent 12 years chasing seniority, maybe it's time to slow down.</p>
+			<p>I only just turned 30, afterall.</p>
 				
 <?php
 	include(path.'_includes/footer.php');
