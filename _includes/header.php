3b9c0f53934053ab42a1a3a4310c73920749be25
Buzz Usborne <hello@buzzusborne.com>
2015-07-05 17:51:47 +1000

    Better OG description

--- _includes/header.php
+++ _includes/header.php
@@ -19,9 +19,9 @@
 	<title>Buzz Usborne - <?php if(isset($navigation) && array_key_exists('this',$navigation)) { echo $navigation['this']['title']; } else { echo "Digital Designer"; } ?></title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="robots" content="index,follow,archive"/>
-	<meta name="Description" content="The portfolio of Digital Art Director, Buzz Usborne" />
+	<meta name="Description" content="The portfolio of Product Designer and Creative Director, Buzz Usborne." />
 	<meta name="Keywords" content="Buzz Usborne director digital design creative art campaign monitor prevue osbourne Pete Peter Graphic Digital Design Portfolio Designer London Sydney" />
-	<meta property="og:description" content="The portfolio of Sydney-based digital Art Director, Buzz Usborne." />
+	<meta property="og:description" content="The portfolio of Product Designer and Creative Director, Buzz Usborne." />
 	<link href="<?php echo path; ?>_assets/gfx/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
 	<link href="<?php echo path; ?>_assets/css/main.css" rel="stylesheet" type="text/css" />
