7073a3dce40cb2258073368fa70d2de947a30a84
Buzz Usborne <hello@buzzusborne.com>
2015-12-28 18:46:49 +1100

    Stab at a new landing

--- index.php
+++ index.php
@@ -1,81 +1,29 @@
-<?php 
+<?php
 	define('path','');
 	$hideNav=TRUE;
+	$bodyclass="dark_and_ominous";
 	$navigation = array(
-		"prev" => array(
-			"path"	=> "/about",
-			"title"	=> "About Buzz"
-		),
 		"next" => array(
-			"path"	=> "casestudy/prevue",
-			"title"	=> "Prevue"
+			"path"	=> "..",
+			"title"	=> "Home"
+		),
+		"prev" => array(
+			"path"	=> "../casestudy/monitor",
+			"title"	=> "Monitor App"
 		)
 	);
 	include(path.'_includes/header.php');
 ?>
-		<div id="about">		
-			<p class="lead">I&rsquo;m a Digital Designer, Founder of Prevue and Head of Design at <a href="https://sendle.com/" target="_blank">Sendle</a>. Previously Product Design Lead at Campaign Monitor, Skype and Creative Director to various digital agencies. <a href="about/">More</a></p>
-			<div id="photo"><a href="about/" title="Read more about me"><img src="_assets/home/buzzusborne_bw.jpg" width="100%" /></a></div>
-		</div>
 		
-		<ul id="caseStudies">
-				<li>
-					<a href="casestudy/prevue/">
-						<div class="image leadImg"><img src="casestudy/_images/prevue/Homepage.jpg" /></div>
-						<h5><span>Prevue</span></h5>
-						<p>Founder &amp; Product Design Lead <em class="special bullet">&#8226;</em> UX <em class="special">/</em> UI <em class="special">/</em> HTML</p>
-					</a>
-				</li>
-				<li>
-					<a href="casestudy/canvas/">
-						<div class="image"><img class="lazyload" data-sizes="auto" data-src="casestudy/_images/canvas/modal_head.jpg" /></div>
-						<h5><span>Canvas</span></h5>
-						<p>Product Design Lead <em class="special bullet">&#8226;</em> UX <em class="special">/</em> UI</p>
-					</a>
-				</li>
-				<li>
-					<a href="casestudy/skype/">
-						<div class="image"><img class="lazyload" data-sizes="auto" data-src="casestudy/_images/skype/desktop_skype.jpg" /></div>
-						<h5><span>Skype</span></h5>
-						<p>Art Director <em class="special bullet">&#8226;</em> Iconography <em class="special">/</em> UI</p>
-					</a>
-				</li>
-				<li>
-					<a href="casestudy/campaignmonitor/">
-						<div class="image"><img class="lazyload" data-sizes="auto" data-src="casestudy/_images/campaignmonitor/signage.jpg" /></div>
-						<h5><span>Campaign Monitor</span></h5>
-						<p>Creative Director <em class="special bullet">&#8226;</em> Branding <em class="special">/</em> Digital <em class="special">/</em> Print <em class="special">/</em> Advertising </p>
-					</a>
-				</li>
-				<li>
-					<a href="casestudy/monitor/">
-						<div class="image"><img class="lazyload" data-sizes="auto" data-src="casestudy/_images/monitor/icons_photo.jpg" /></div>
-						<h5><span>Monitor App</span></h5>
-						<p>Product Design Lead <em class="special bullet">&#8226;</em> Iconography <em class="special">/</em> UX <em class="special">/</em> UI</p>
-					</a>
-				</li>
-				<li class="soon">
-					<h5><span>Create <small>+</small> Send<div class="strikethrough">&nbsp;</div></span></h5>
-					<p>Product Design Lead &mdash; Coming soon</p>
-				</li>
-				<li class="soon">
-					<h5><span>Postbox Party<div class="strikethrough">&nbsp;</div></span></h5>
-					<p>Art Director &mdash; Coming soon</p>
-				</li>
-				<li class="soon">
-					<h5><span>Toni <small>+</small> Guy<div class="strikethrough">&nbsp;</div></span></h5>
-					<p>Art Director &mdash; Coming soon</p>
-				</li>
-				<li class="soon">
-					<h5><span>The Argyle<div class="strikethrough">&nbsp;</div></span></h5>
-					<p>Art Director &mdash; Coming soon</p>
-				</li>
-				<li class="soon">
-					<h5><span>Skype for Business<div class="strikethrough">&nbsp;</div></span></h5>
-					<p>Lead Visual Designer &mdash; Coming soon</p>
-				</li>
-		</ul>
-				
+		<div id="about">		
+			<h3>About.</h3>
+			<p class="lead">I&rsquo;m Buzz, a designer based in Sydney. I&rsquo;m deeply passionate about problem solving and enjoy varied, challenging digital work. I create thoughtful, beautiful and useful product experiences through design, code and strategy.</p>
+							
+			<h6>Work</h6>
+			<p>I currently work as the Head of Product at <a href="#">Sendle</a>, I am also the Founder of <a href="#">Prevue</a>. Until 2015, I was the UX Director at software startup, <a href="#">Campaign Monitor</a> where I ran several teams of designers &mdash; and also designed a number of major features. I've also had the pleasure of working with Skype, Facebook and various small startups as a UX/UI designer.</p>
+			
+			<p>Before my time in Sydney, I spent almost 10 years in permanent and freelance positions at digital agencies in London. In roles ranging from Graphic Designer to Creative Director &mdash; I've designed websites and apps for fashion, retail and media brands.</p>
+					
 <?php
 	include(path.'_includes/footer.php');
 ?>
