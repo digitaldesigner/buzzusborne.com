97efdaca55d501ab59c9f7d4a2edcf5b7bcc96d4
Buzz Usborne <hello@buzzusborne.com>
2017-02-15 08:34:04 +1100

    New interview

--- _includes/recent.php
+++ _includes/recent.php
@@ -10,6 +10,7 @@
 				</div>
 				<div class="col left">
 					<dt>Recent Interviews</dt>
+						<dd><a hfref="http://nicelydone.club/interviews/buzz-usborne/">Nicely Done</a></dd>
 						<dd><a href="https://uigarage.net/blog-post/learn-atlassians-lead-designer-buzz-usborne/">UI Garage</a></dd>
 						<dd><a href="http://www.intelligentproductdesign.com/buzz-usborne/">Intelligent Product Design</a></dd>
 						<dd><a href="http://www.tractor.edu.au/news/industry-interview-buzz-usborne">Tractor: Industry Interview</a></dd>
@@ -17,16 +18,18 @@
 				</div>
 				<div class="col middle">			
 					<dt>Writing</dt>
-						<dd><a href="https://medium.com/prevue-app/god-is-in-the-details-bc3a9a9a5d88">&ldquo;God is in the Details&rdquo;</a></dd>
-						<dd><a href="https://medium.com/prevue-app/a-designer-s-guide-to-great-client-presentations-3fdb9871696b">&ldquo;A Guide to Client Presentations&rdquo;</a></dd>
+						<dd><a href="https://medium.com/@buzzusborne/where-to-find-inspiration-2c39470f3872#.4qc1qce4j">&ldquo;Where to find inspiration&rdquo;</a></dd>
+						<dd><a href="https://medium.com/prevue-app/god-is-in-the-details-bc3a9a9a5d88">&ldquo;God is in the details&rdquo;</a></dd>
+						<dd><a href="https://medium.com/prevue-app/a-designer-s-guide-to-great-client-presentations-3fdb9871696b">&ldquo;A guide to client presentations&rdquo;</a></dd>
 						<dd><a href="https://medium.com/@buzzusborne/the-intersection-ba071f99d9d4">&ldquo;The Intersection&rdquo;</a></dd>
-						<dd><a href="https://medium.com/prevue-app/the-art-of-suggestion-e5f1bcbadca2#.4ka05pyhb">&ldquo;The Art of Suggestion&rdquo;</a></dd>
+						<dd><a href="https://medium.com/prevue-app/the-art-of-suggestion-e5f1bcbadca2#.4ka05pyhb">&ldquo;The art of suggestion&rdquo;</a></dd>
 				</div>
 				<div class="col right">
 					<dt>Contact</dt>
 						<dd><a href="http://linkedin.com/in/buzzusborne/">LinkedIn</a></dd>
 						<dd><a href="http://medium.com/@buzzusborne/">Medium</a></dd>
 						<dd><a href="http://twitter.com/@buzzusborne/">Twitter</a></dd>
-						<dd><a href="mailto:hello@buzzusborne.com">Email me</a></dd>
+						<dd><a href="https://www.instagram.com/buzz/">Instagram</a></dd>
+						<dd><a href="mailto:hello@buzzusborne.com">Email</a></dd>
 				</div>
 			</dl>
