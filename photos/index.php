b804ff9cfd6a49763834ba99692571c55c8a1fdd
Buzz Usborne <hello@buzzusborne.com>
2017-11-22 22:38:28 +1100

    Allowing individual images on Photos feed
    By instagram URL ID

--- photos/index.php
+++ photos/index.php
@@ -15,12 +15,22 @@
 		)
 	);
 	
+	$singlePhoto = FALSE;
+	if(isset($_GET) && $_GET['id']){
+		$singlePhoto = $_GET['id'];
+	}
+
 	// Instagram stuffs
 	$access_token="10107.ffd12dd.9c84dc0a5ade4640b35a240575751778";
 	$photo_count=10;
+	if($singlePhoto) { $photo_count = 50; }
+	$json=NULL;
 	$json_link="https://api.instagram.com/v1/users/self/media/recent/?";
 	$json_link.="access_token={$access_token}&count={$photo_count}";
 	$json=file_get_contents($json_link);
+	
+	if(!$json) { header('Location: https://instagram.com/buzz/'); die(); }
+	
 	$obj=json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
 	
 	// Regular header
@@ -42,56 +52,104 @@
 			
 			$i=1;
 			foreach ($obj['data'] as $post):
-				
-				if($i % 2 == 0){
-					
-				} else {
-					echo "		<div class=\"wrap\">\n\n";
-				}
-				
-				echo "			<li>";
-				echo "\n				<a href='{$post['link']}' target='_blank' title=\"{$post['caption']['text']}\">
-					<img class='img-responsive photo-thumb' src='{$post['images']['standard_resolution']['url']}' />
-				</a>";
 
-				echo "\n				<p>";
+				if($singlePhoto) {
+					// Show a single photo
+					$canalonical = explode("/",$post['link']);
+					$canalonical = $canalonical[4];
+					if($canalonical != $singlePhoto) { 
+						break;
+					} else {
+
+						echo "			<li>";
+						echo "\n				<a href='{$post['link']}' target='_blank' title=\"{$post['caption']['text']}\">
+							<img class='img-responsive photo-thumb' src='{$post['images']['standard_resolution']['url']}' />
+						</a>";
+
+						echo "\n				<p>";
 				
-				$post_like = NULL;
-				$post_loc = NULL;
-				if(
-					array_key_exists('location',$post)
-					&& count($post['location'])
-					&& $post['location']['latitude']
-					&& $post['location']['longitude']
-				) {
-					$locName = "Location";
-					if($post['location']['name']){
-						$firstLoc = explode(",",$post['location']['name']);
-						$locName = $firstLoc[0];
-					}
+						$post_like = NULL;
+						$post_loc = NULL;
+						if(
+							array_key_exists('location',$post)
+							&& count($post['location'])
+							&& $post['location']['latitude']
+							&& $post['location']['longitude']
+						) {
+							$locName = "Location";
+							if($post['location']['name']){
+								$firstLoc = explode(",",$post['location']['name']);
+								$locName = $firstLoc[0];
+							}
 					
-					$post_loc = "<a href=\"https://www.google.com/maps/preview/@".$post['location']['latitude'].",".$post['location']['longitude'].",18z\" target=\"_blank\" class=\"location\">{$locName}</a>";
+							$post_loc = "<a href=\"https://www.google.com/maps/preview/@".$post['location']['latitude'].",".$post['location']['longitude'].",18z\" target=\"_blank\" class=\"location\">{$locName}</a>";
+						}
+						$pic_like_count=$post['likes']['count'];
+						if($pic_like_count>10){
+							$post_like = "<span class=\"likes\"><strong>&#10084;</strong> {$pic_like_count}</span>";
+						}
+				
+						if($post_loc || $post_like){ echo $post_loc.$post_like; }
+				
+						echo "</p>";
+						echo "\n			</li>\n";
+
+
+					}
+				} else {
+					// Show stream (all)
+					if($i % 2 == 0){
+					
+					} else {
+						echo "		<div class=\"wrap\">\n\n";
+					}
+				
+					echo "			<li>";
+					echo "\n				<a href='{$post['link']}' target='_blank' title=\"{$post['caption']['text']}\">
+						<img class='img-responsive photo-thumb' src='{$post['images']['standard_resolution']['url']}' />
+					</a>";
+
+					echo "\n				<p>";
+				
+					$post_like = NULL;
+					$post_loc = NULL;
+					if(
+						array_key_exists('location',$post)
+						&& count($post['location'])
+						&& $post['location']['latitude']
+						&& $post['location']['longitude']
+					) {
+						$locName = "Location";
+						if($post['location']['name']){
+							$firstLoc = explode(",",$post['location']['name']);
+							$locName = $firstLoc[0];
+						}
+					
+						$post_loc = "<a href=\"https://www.google.com/maps/preview/@".$post['location']['latitude'].",".$post['location']['longitude'].",18z\" target=\"_blank\" class=\"location\">{$locName}</a>";
+					}
+					$pic_like_count=$post['likes']['count'];
+					if($pic_like_count>10){
+						$post_like = "<span class=\"likes\"><strong>&#10084;</strong> {$pic_like_count}</span>";
+					}
+				
+					if($post_loc || $post_like){ echo $post_loc.$post_like; }
+				
+					echo "</p>";
+					echo "\n			</li>\n";
+				
+					if($i % 2 == 0){
+						echo "\n		</div>\n";
+					}
+					$i++;
 				}
-				$pic_like_count=$post['likes']['count'];
-				if($pic_like_count>10){
-					$post_like = "<span class=\"likes\"><strong>&#10084;</strong> {$pic_like_count}</span>";
-				}
-				
-				if($post_loc || $post_like){ echo $post_loc.$post_like; }
-				
-				echo "</p>";
-				echo "\n			</li>\n";
-				
-				if($i % 2 == 0){
-					echo "\n		</div>\n";
-				}
-				
-				
-				$i++;
 			endforeach;
+			
+			if($singlePhoto){ echo "		<div class=\"wrap\" style=\"margin: 40px 0 -40px;\"><!-- Inline style, so sue me -->\n"; }
+			echo "			<li class=\"more\"><a href=\"https://instagram.com/buzz/\" target=\"_blank\" title=\"See more on Instagram\">See more @buzz</a></li>";
+			if($singlePhoto){ echo "\n		</div>\n"; }
 
 ?>
-		<li class="more"><a href="https://instagram.com/buzz/" target="_blank" title="See more on Instagram">See more @buzz</a></li>
+		
 	</ul>
 			
 	<div class="container">
