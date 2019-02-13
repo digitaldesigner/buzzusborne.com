31e068dfb4ad758f96cef819646c2bdcb7cc0f01
Buzz <hello@buzzusborne.com>
2019-02-14 09:57:43 +1100

    Local env changes

--- _includes/header.php
+++ _includes/header.php
@@ -1,7 +1,7 @@
 <?php
 
-	if($_SERVER['SERVER_PORT']!='443' && $_SERVER['HTTP_HOST']!="portfolio"){
-		header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+	if($_SERVER['SERVER_PORT']!='443' && $_SERVER['HTTP_HOST']!=("portfolio" || "portfolio:8888")){
+    header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 		exit();
 	}
 	$fullURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
